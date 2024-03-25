936ed3f5734f2f69ab8632036664f37cb151c8b9
808raf <102700868+808raf@users.noreply.github.com>
2024-03-24 20:40:01 -1000

    Add modal popup once "Add Book" button is clicked
    The modal UI is a simple way to add a book to the library array.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const addBookToLibrary = (book) => {
   return myLibrary.push(book);
 };
 
-const hobbit = new Book("Hobbit", "J.R.R. Tolken", 295, "not read yet");
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
 const donQuixote = new Book("Don Quixote", "Miguel de Cervantes", 1072, "read");
 const alice = new Book(
   "Alice's Adventures in Wonderland",
@@ -28,13 +28,15 @@ addBookToLibrary(donQuixote);
 addBookToLibrary(alice);
 console.log(myLibrary);
 
-myLibrary.forEach((book) => {
+myLibrary.forEach((book, idx) => {
   const table = document.querySelector("tbody");
   const row = document.createElement("tr");
   const bookTitle = document.createElement("td");
   const bookAuthor = document.createElement("td");
   const bookPages = document.createElement("td");
   const bookStatus = document.createElement("td");
+
+  row.setAttribute("data-bookid", idx);
 
   bookTitle.innerText = `${book.title}`;
   bookAuthor.innerText = `${book.author}`;
@@ -44,3 +46,29 @@ myLibrary.forEach((book) => {
   table.appendChild(row);
   row.append(bookTitle, bookAuthor, bookPages, bookStatus);
 });
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.querySelector(".addBookBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
