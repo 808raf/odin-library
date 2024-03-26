11e581a2f99c4d9a9de626d1919f9a47ac872854
808raf <102700868+808raf@users.noreply.github.com>
2024-03-25 17:47:22 -1000

    Refactor table update
    Previously the table was updated by adding in a row, which would cause
    a conflict with also having to update the array. Now only an array
    update is needed to call the updateTable function and a new table is
    made.

--- script.js
+++ script.js
@@ -89,64 +89,14 @@ const createDefaultTable = () => {
   });
 };
 
-const updateTable = (book) => {
-  const table = document.querySelector("tbody");
-  const row = document.createElement("tr");
-  const bookTitle = document.createElement("td");
-  const bookAuthor = document.createElement("td");
-  const bookPages = document.createElement("td");
-  const bookStatus = document.createElement("td");
-  const bookDelete = document.createElement("td");
+const updateTable = () => {
+  const currTable = document.querySelector("tbody");
+  currTable.remove();
 
-  row.setAttribute("data-bookid", myLibrary.length - 1);
-  row.classList.add("book");
-
-  bookTitle.innerText = `${book.title}`;
-  bookAuthor.innerText = `${book.author}`;
-  bookPages.innerText = `${book.pages}`;
-  bookStatus.innerText = `${book.read}`;
-  bookDelete.innerHTML = `<button class="bin-button" data-delete-bookid="${
-    myLibrary.length - 1
-  }">
-    <svg
-      class="bin-top"
-      viewBox="0 0 39 7"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <line y1="5" x2="39" y2="5" stroke="white" stroke-width="4"></line>
-      <line
-        x1="12"
-        y1="1.5"
-        x2="26.0357"
-        y2="1.5"
-        stroke="white"
-        stroke-width="3"
-      ></line>
-    </svg>
-    <svg
-      class="bin-bottom"
-      viewBox="0 0 33 39"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <mask id="path-1-inside-1_8_19" fill="white">
-        <path
-          d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
-        ></path>
-      </mask>
-      <path
-        d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
-        fill="white"
-        mask="url(#path-1-inside-1_8_19)"
-      ></path>
-      <path d="M12 6L12 29" stroke="white" stroke-width="4"></path>
-      <path d="M21 6V29" stroke="white" stroke-width="4"></path>
-    </svg>
-  </button>`;
-
-  table.appendChild(row);
-  row.append(bookTitle, bookAuthor, bookPages, bookStatus, bookDelete);
+  const newTable = document.createElement("tbody");
+  const container = document.querySelector(".book-table");
+  container.append(newTable);
+  createDefaultTable();
 };
 
 createDefaultTable();
@@ -196,9 +146,9 @@ const handleFormSubmit = (e) => {
   let book = new Book(title.value, author.value, pages.value, status.value);
   addBookToLibrary(book);
 
-  clearForm();
-  updateTable(book);
   modal.style.display = "none";
+  clearForm();
+  updateTable();
 
   console.log(myLibrary);
 };
